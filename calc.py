270ba516b839729e8e0e22da2bf252bb8df1d200
lixiang <lixiang941@163.com>
2019-09-26 09:27:51 +0800

    tijiao

---
+++ calc.py
@@ -0,0 +1,2 @@
+print(2**10)
+print('hello,wrold')
---
+++ hello.go
@@ -0,0 +1,22 @@
+// package main
+
+// import "fmt"
+// func main(){
+// 	var age int;
+// 	age =1;
+// 	fmt.Println(age)
+// }
+
+package main
+
+import "fmt"
+ var a int =10
+// var b =20
+//c := 30
+//var c bool
+
+func main() {
+	// c:=30
+	// fmt.Println(a,b,c);
+	fmt.Println("hello, world", a)
+}
